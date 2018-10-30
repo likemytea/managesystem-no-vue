dbbb840a77b55bfd0916cf72e699e022c3b65b44
huayu <huayu@DESKTOP-N5K2GI1>
2018-10-30 18:51:57 +0800

    aaa

---
+++ src/main/java/com/chenxing/managesystem/ManagesystemApplication.java
@@ -0,0 +1,22 @@
+package com.chenxing.managesystem;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+/**
+ * 为了适应 ie8，不使用vue的版本
+ * 
+ * 
+ */
+
+@EnableFeignClients
+@SpringBootApplication
+@MapperScan("com.chenxing.managesystem.mapper") // 将项目中对应的mapper类
+public class ManagesystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagesystemApplication.class, args);
+	}
+}
