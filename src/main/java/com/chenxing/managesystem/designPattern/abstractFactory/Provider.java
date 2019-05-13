fd30674c78866d5f341013f2ddcd4b1632e6721b
huayu <huayu@DESKTOP-N5K2GI1>
2019-05-13 14:22:17 +0800

    aa

---
+++ src/main/java/com/chenxing/managesystem/designPattern/abstractFactory/Provider.java
@@ -0,0 +1,7 @@
+package com.chenxing.managesystem.designPattern.abstractFactory;
+
+import com.chenxing.managesystem.designPattern.ordinaryFactory.MyInterface;
+
+public interface Provider {
+	public MyInterface produce();
+}
