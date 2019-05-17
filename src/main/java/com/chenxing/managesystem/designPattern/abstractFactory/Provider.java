9c1259c27d27c780f2de3f658a602698e7442ac6
huayu <huayu@DESKTOP-N5K2GI1>
2019-05-17 13:17:21 +0800

    aaa

--- src/main/java/com/chenxing/managesystem/designPattern/abstractFactory/Provider.java
+++ src/main/java/com/chenxing/managesystem/designPattern/abstractFactory/Provider.java
@@ -3,7 +3,7 @@ package com.chenxing.managesystem.designPattern.abstractFactory;
 import com.chenxing.managesystem.designPattern.ordinaryFactory.MyInterface;
 
 /**
- * 抽象工厂1
+ * 抽象工厂1:和静态工厂相比，主要是实现了对修改关闭，对扩展开放
  */
 public interface Provider {
 	public MyInterface produce();
